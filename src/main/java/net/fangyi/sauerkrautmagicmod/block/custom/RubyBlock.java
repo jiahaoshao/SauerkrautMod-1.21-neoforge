0030dd8db9c267b70347acad425e73858698e11d
jiahaoshao <2370145097@qq.com>
2025-03-17 11:31:21 +0800

    一点修改

--- src/main/java/net/fangyi/sauerkrautmagicmod/block/custom/RubyBlock.java
+++ src/main/java/net/fangyi/sauerkrautmagicmod/block/custom/RubyBlock.java
@@ -1,12 +1,20 @@
 package net.fangyi.sauerkrautmagicmod.block.custom;
 
 import com.jcraft.jorbis.DspState;
+import com.mojang.serialization.MapCodec;
+import net.minecraft.world.level.block.AnvilBlock;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.FallingBlock;
 
-public class RubyBlock extends Block {
-
+public class RubyBlock extends FallingBlock {
+    public static final MapCodec<RubyBlock> CODEC = simpleCodec(RubyBlock::new);
     public RubyBlock(Properties properties) {
         super(properties);
+    }
+
+    @Override
+    protected MapCodec<? extends FallingBlock> codec() {
+        return CODEC;
     }
 }
